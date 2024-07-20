0cfcb165f6d8dcb9c370b23526ff63a34afc20f0
KendallBranchett <1325936293@qq.com>
2024-07-20 15:56:53 +0800

    feature: 初始图表结构

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-5 flex overflow-hidden">
+  <div class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-5 flex overflow-hidden"
+    v-if="data">
     <!-- 左 -->
     <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
       <!-- 横向柱状图 -->
-      <HorizontalBar class="h-1/3 box-border pb-4" />
+      <HorizontalBar class="h-1/3 box-border pb-4" :data="data.regionData" />
       <!-- 雷达图 -->
-      <RadarBar class="h-1/3 box-border pb-4" />
+      <RadarBar class="h-1/3 box-border pb-4" :data="data.riskData" />
       <!-- 数据传递关系图 -->
       <Relation class="h-1/3 " />
     </div>
     <!-- 中 -->
     <div class="w-1/2 mr-2 flex flex-col">
       <!-- 数据总览图 -->
-      <TotalData class="bg-opcity-50 bg-slate-800 p-3 "/>
+      <TotalData class="bg-opcity-50 bg-slate-800 p-3 " />
       <!-- 地图可视化 -->
-      <MapChart class="bg-opcity-50 bg-slate-800 p-3 mt-4 flex-1"/>
+      <MapChart class="bg-opcity-50 bg-slate-800 p-3 mt-4 flex-1" />
     </div>
     <!-- 右 -->
     <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
@@ -23,7 +24,7 @@
       <!-- 环形图 -->
       <RingBar class="h-1/3 box-border pb-4" />
       <!-- 文档云图 -->
-      <WordCloud class="h-1/3"/>
+      <WordCloud class="h-1/3" />
     </div>
   </div>
 </template>
